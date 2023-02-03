d9365b9c9f856955733f42eaabc8cda568e0e9fb
Alma Cleto <emscleto11@gmail.com>
2023-02-03 19:48:30 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Hello from './Hello';
 import './css/App.css';
-
+import Count from './Count';
 
 function App() {
   return (
     <div className="App">
       <h1>This is the App.js</h1>
       <Hello></Hello>
+      <Count></Count>
     </div>
   );
 }
